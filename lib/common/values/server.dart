185d16f4eadc403aa24bfc9422694e0e4ea154e9
Limpid-8818 <157373409+Limpid-8818@users.noreply.github.com>
2026-03-25 13:27:34 +0800

    Update server.dart

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
 class ServerConfig {
-  static const String BASE_URL =
-      "http://10.0.2.2:4523/m1/7772857-7518891-default/api/v1";
+  static const String BASE_URL = "http://go/server/address:xxxx/api/v1";
   static const int CONNECT_TIMEOUT = 10000;
   static const int RECEIVE_TIMEOUT = 10000;
 }
