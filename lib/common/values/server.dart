5a9e45ef3a0560d82312a73b9ef294169eccc325
Limpid <Limpid8818@outlook.com>
2026-03-31 21:02:14 +0800

    chore: modify timeout limitition

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -4,6 +4,6 @@ class ServerConfig {
   //static const String BASE_URL = "http://go/server/address:xxxx/api/v1";
   static const String BASE_URL =
       "http://10.0.2.2:4523/m1/7557631-7295075-default/api/v1";
-  static const int CONNECT_TIMEOUT = 10000;
-  static const int RECEIVE_TIMEOUT = 10000;
+  static const int CONNECT_TIMEOUT = 60000;
+  static const int RECEIVE_TIMEOUT = 60000;
 }
